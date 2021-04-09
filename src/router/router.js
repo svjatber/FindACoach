eebacf480822a8eb2c72ebd50a993ef1fd140927
svjatber <svjat.ber3003@gmail.com>
2021-04-09 20:21:08 +0300

    'withHah'

--- src/router/router.js
+++ src/router/router.js
@@ -1,8 +1,7 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter} from "vue-router";
 
 import store from '../store/index'
 const router = createRouter({
-    history: createWebHistory(),
     routes: [
         {
           path: '/', redirect: '/coaches'
